d3e1934c09d642bf47fd04e16ac9d563566f2247
Johnny <yufengzhang483@gmail.com>
2024-03-28 17:50:05 -0400

    refine SoftMax Algorithm

--- src/Layer_class.py
+++ src/Layer_class.py
@@ -13,8 +13,8 @@ class Layer:
         self.units = units
         self.activation = activation
         self.name = name
-        self.Weights = None
-        self.Biases = None
+        self.Weights = np.zeros(units)
+        self.Biases = np.zeros(units)
 
     def compute_layer(self, a_in: np.ndarray) -> np.ndarray:
         # init variables
@@ -31,22 +31,12 @@ class Layer:
         if self.activation == 'softmax':
             a_out = np.zeros(self.units)
             z = np.zeros(self.units)
-            results=np.zeros()
             # The first loop compute all inputs for sigmoid
-            for i in range(self.units):
-                z[i] = linear_function(a_in, self.Weights[:, i], self.Biases[i])
-            # 2nd loop compute output of each nodes
-            for j in range(self.units):
-                a_out[i] = sigmoid_function(z)
-            # third loop compute weighted outputs of each options
-            for k in range(self.units):
-                
-        return a_out
+            z = linear_function(a_in, self.Weights, self.Biases)
+            exp_z = np.exp(z)
+            a_out = exp_z / np.sum(exp_z)
 
-    @staticmethod
-    def sigmoid_function(z):
-        g = 1 / (1 + np.exp(-z))
-        return g
+        return a_out
 
     def set_weights(self, w: np.ndarray, b: np.ndarray):
         self.Weights = w
