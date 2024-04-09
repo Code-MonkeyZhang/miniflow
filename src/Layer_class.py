4787736f5f1b05fb8a2f6663ff5328e95314dc94
Johnny <yufengzhang483@gmail.com>
2024-04-09 16:50:07 -0400

    implement backprop, it can compile

--- src/Layer_class.py
+++ src/Layer_class.py
@@ -8,13 +8,14 @@ class Layer:
     layer Class
     """
 
-    def __init__(self, units: int, activation: str, name='layer'):
+    def __init__(self, units: int, activation: str, name='layer', input_shape: int = 0):
         # init variables
         self.units = units
         self.activation = activation
         self.name = name
-        self.Weights = np.zeros(units)
+        self.Weights = np.zeros((units, input_shape))
         self.Biases = np.zeros(units)
+        self.input_shape = input_shape
 
     def compute_layer(self, a_in: np.ndarray) -> np.ndarray:
         # init variables
@@ -31,25 +32,93 @@ class Layer:
         if self.activation == 'softmax':
             a_out = np.zeros(self.units)
             z = np.zeros(self.units)
-            # The first loop compute all inputs for sigmoid
+
             z = linear_function(a_in, self.Weights, self.Biases)
             exp_z = np.exp(z)
             a_out = exp_z / np.sum(exp_z)
 
         return a_out
 
+    """
+    This function performs Two major tasks:
+    - obtain weights and bias
+    - do gradient descent
+    """
+
+    def train_layer(self, prev_layer_output, curr_layer_output, label, learningRate,
+                    backprop_gradient) -> np.ndarray:
+        # 暂且是这样
+        cost_func_gradient = np.subtract(curr_layer_output, label)
+        # obtain gradients of weights and bias for updates
+        dL_dw, dj_db = self.compute_gradient(prev_layer_output, cost_func_gradient, backprop_gradient)
+
+        # 根据 chain rule, 传给上一层的gradient应该是:
+        # dl/da = dl/ds * ds/da
+        if self.activation == "softmax":
+            backprop_gradient = np.dot(cost_func_gradient, self.Weights)
+
+        # do gradient descent, update gradient
+        self.Weights -= learningRate * dL_dw
+        self.Biases -= learningRate * dj_db
+
+        return backprop_gradient
+
+    def compute_gradient(self, prev_layer_output, cost_func_gradient, backprop_gradient) -> np.ndarray:
+        if self.activation == "softmax":
+            dL_dz = cost_func_gradient
+            # linear 的一阶导是x,也就是这一层的输入
+            dz_dw = prev_layer_output
+            dL_dw = np.dot(dz_dw.T, dL_dz)
+            # multiply gradients with the backprop gradients from the prev layer
+            # if this is the last layer, backprop gradients are all 1s
+            dL_dw = np.multiply(backprop_gradient, dL_dw.T)
+
+            dL_db = np.mean(dL_dz, axis=0)
+        if self.activation == "relu":
+            # linear 的一阶导是x,也就是这一层的输入
+            dz_dw = prev_layer_output
+            # relu_derivative = (dz_dw > 0).astype(float)
+            dL_dz = backprop_gradient
+            dL_dw = np.dot(dL_dz.T, dz_dw)
+
+            dL_db = np.mean(dL_dz)
+
+        return dL_dw, dL_db
+
+    def compute_cost_gradient(self, prediction, label):
+        if self.activation == "softmax":
+            cost_gradient = np.subtract(prediction, label)
+
+        return cost_gradient
+
     def set_weights(self, w: np.ndarray, b: np.ndarray):
         self.Weights = w
         self.Biases = b
+
+    def get_weights(self) -> np.ndarray:
+        return self.Weights
+
+    def set_random_weights(self):
+        self.Weights = np.random.randn(*self.Weights.shape)
+        self.Biases = np.random.randn(*self.Biases.shape)
 
 
 class FlattenLayer(Layer):
     def __init__(self, input_shape, name='Flatten'):
         # 由于Flatten层不需要units和activation，我们可以传递默认值或None
-        super().__init__(units=0, activation=None, name=name)
+        super().__init__(units=0, name=name, activation="Flatten")
         self.input_shape = input_shape
+        self.activation = "Flatten"
 
     def compute_layer(self, input_array):
         num_elements = np.prod(input_array.shape[1:])
-        output_array = input_array.reshape((input_array.shape[0], num_elements))
+        output_array = input_array.reshape(
+            (input_array.shape[0], num_elements))
         return output_array
+
+    def train_layer(self, prev_layer_output, curr_layer_output, label, learningRate,
+                    backprop_gradient) -> np.ndarray:
+        pass
+
+    def set_random_weights(self):
+        pass
--- src/Model_class.py
+++ src/Model_class.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pdb;
 from src.Layer_class import *
 
 """
@@ -6,22 +8,73 @@ Model Class
 ======================================================================
 """
 
+
 class Model:
-    def __init__(self, dense_array: List[Layer], name='model') -> None:
+    def __init__(self, dense_array: List[Layer], cost, name='model') -> None:
         self.dense_array = dense_array
+        self.layers_output = []
         self.name = name
+        self.cost = cost
 
     # Iterate through each layer, and puts its output to the next layer
     def predict(self, x: np.ndarray) -> np.ndarray:
+        prev_layer_output = x
         for dense_layer in self.dense_array:
-            x = dense_layer.compute_layer(x)
-        return x
+            self.layers_output.append((dense_layer, prev_layer_output))
+            layer_output = dense_layer.compute_layer(prev_layer_output)
+            prev_layer_output = layer_output
+            if np.any(np.isnan(prev_layer_output)):
+                print(f"NaN detected in layer_output")
+                a = 1 + 1
+                print(a)
+        return prev_layer_output
 
-    # TBD
-    # def fit(self, X_train, y_train, epochs):
-    #     # perform backward prop
-    #     pass
-    # def evaluate(self):
-    #     pass
-    # def get_weights(self):
-    #     pass
+    def fit(self, X_train, y_train, learningRate, epochs):
+        # perform backward prop
+        print("Start Training")
+        for epoch in range(epochs):
+            print("Epoch {}/{}".format(epoch + 1, epochs))
+
+            # In each epoch we iterate through each training example
+            for i in range(X_train.shape[0]):
+
+                # Make sure to clear the layers_output at the start of processing each sample
+                self.layers_output.clear()
+
+                train_example = X_train[i][np.newaxis, ...]
+                label = y_train[i]
+
+                # do forward prop to compute lost
+                # and save output of each layer
+                prediction = self.predict(train_example)
+
+                loss = self.compute_loss(prediction, label)
+                print("Loss: {}".format(loss))
+
+                # start training
+                # init backprop_gradient as all ones, when training from the last layer(output layer)
+                backprop_gradient = np.ones(self.dense_array[-1].get_weights().shape)
+
+                # reverse iterate the layers
+                for layer, prev_layer_output in reversed(self.layers_output):
+                    if layer.activation == "Flatten":
+                        break
+                    backprop_gradient = layer.train_layer(prev_layer_output, prediction, label, learningRate,
+                                                          backprop_gradient)
+
+    def compute_loss(self, prediction, target):
+        # Compute the loss between the predicted output and the target
+        # You can use any loss function here, such as mean squared error
+        loss = np.mean((prediction - target))
+        return loss
+
+    def set_rand_weight(self):
+        for layer in self.dense_array:
+            layer.set_random_weights()
+        print("Set random weights Complete")
+
+# def evaluate(self):
+#     pass
+# def get_weights(self):
+#     pass
+# use forward prop to get prediction
