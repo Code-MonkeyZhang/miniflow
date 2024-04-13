31d1397ef7a06b3cd3527835a7a7e0eafb1ca9d9
Yufeng(Jonathan) Zhang <56613491+Code-MonkeyZhang@users.noreply.github.com>
2024-04-13 12:32:13 -0400

    add backprop, achive Minst dataset learninfg at 87% accuracy (#8)
    * update gitignore
    
    * change how to import activation
    
    * change YNN to Ynn
    
    * fix import problem for multi linear regression test
    
    * change softmax weights shape from 128 to 25
    
    * add learning test for YNN
    
    * add backprop prototype to test YNN in small scale
    
    * modify linear activition
    
    * modify other test files in general
    
    * implement backprop, it can compile
    
    * modify softmax calculation method to avoid overflow
    
    * modify softmax loss calculation method, and add some comments
    
    * small changes in learning test
    
    * modify comments
    
    * fix label to one hot encoding, Ynn Stage one success
    
    * add accuracy calculations

--- tests/forwardProp_test/getSoftMax_weights.py
+++ tests/forwardProp_test/getSoftMax_weights.py
@@ -10,7 +10,7 @@ x_train, x_test = x_train / 255.0, x_test / 255.0  # 归一化
 
 model = models.Sequential([
     layers.Flatten(input_shape=(28, 28)),
-    layers.Dense(128, activation='relu'),
+    layers.Dense(25, activation='relu'),
     layers.Dense(10, activation='softmax')
 ])
 
@@ -22,37 +22,21 @@ model.fit(x_train, y_train, epochs=5)
 model.evaluate(x_test, y_test)
 predictions = model.predict(x_test)
 
-layers = model.layers
-for i, layer in enumerate(model.layers):
-    weights = layer.get_weights()  # This gets both weights and biases, if they exist.
-    if len(weights) > 0:  # Check if the layer has weights
-        weight_file_path = f'layer_{i}_weights.npy'
-        # np.save(weight_file_path, weights[0])  # Save the weights
-        print(f"Saved weights to {weight_file_path}")
+# layers = model.layers
+[layer1, layer2] = model.layers[1:]
+W1, b1 = layer1.get_weights()
+W2, b2 = layer2.get_weights()
+print(W1.shape, W2.shape, b1.shape, b2.shape)
 
-        if len(weights) > 1:  # Check if the layer has biases
-            bias_file_path = f'layer_{i}_biases.npy'
-            # np.save(bias_file_path, weights[1])  # Save the biases
-            print(f"Saved biases to {bias_file_path}")
-
-# 选择要可视化的样本数量
-num_samples = 20
-fig, axes = plt.subplots(1, num_samples, figsize=(20, 2))
-
-for i, ax in enumerate(axes):
-    # 选择一个随机索引
-    idx = np.random.choice(x_test.shape[0])
-
-    # 绘制图像
-    ax.imshow(x_test[idx], cmap='gray')
-    ax.axis('off')
-
-    # 获取真实标签和预测标签
-    true_label = y_test[idx]
-    predicted_label = np.argmax(predictions[idx])
-
-    # 设置标题显示真实标签和预测标签
-    ax.set_title(f"True: {true_label}\nPred: {predicted_label}")
-
-plt.tight_layout()
-plt.show()
+#
+# for i, layer in enumerate(model.layers):
+#     weights = layer.get_weights()  # This gets both weights and biases, if they exist.
+#     if len(weights) > 0:  # Check if the layer has weights
+#         weight_file_path = f'layer_{i}_weights.npy'
+#         # np.save(weight_file_path, weights[0])  # Save the weights
+#         print(f"Saved weights to {weight_file_path}")
+#
+#         if len(weights) > 1:  # Check if the layer has biases
+#             bias_file_path = f'layer_{i}_biases.npy'
+#             # np.save(bias_file_path, weights[1])  # Save the biases
+#             print(f"Saved biases to {bias_file_path}")
