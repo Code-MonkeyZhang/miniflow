5b6d3f55f638761bd2b19a0a2757ac5510aa7680
Yufeng(Jonathan) Zhang <56613491+Code-MonkeyZhang@users.noreply.github.com>
2024-04-13 12:32:13 -0400

    add backprop, achive Minst dataset learninfg at 87% accuracy (#8)
    * update gitignore
    
    * change how to import activation
    
    * change YNN to Ynn
    
    * fix import problem for multi linear regression test
    
    * change softmax weights shape from 128 to 25
    
    * add learning test for YNN
    
    * add backprop prototype to test YNN in small scale
    
    * modify linear activition
    
    * modify other test files in general
    
    * implement backprop, it can compile
    
    * modify softmax calculation method to avoid overflow
    
    * modify softmax loss calculation method, and add some comments
    
    * small changes in learning test
    
    * modify comments
    
    * fix label to one hot encoding, Ynn Stage one success
    
    * add accuracy calculations

--- tests/forwardProp_test/SoftMax_test.py
+++ tests/forwardProp_test/SoftMax_test.py
@@ -1,10 +1,30 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
+import sys
+
+
+# append WorkSpace to sys.path
+def find_project_root(current_dir, root_dir_name='Ynn'):
+    """向上遍历目录树以找到项目根目录"""
+    while True:
+        parent_dir, dir_name = os.path.split(current_dir)
+        if dir_name == root_dir_name:
+            return current_dir
+        if parent_dir == current_dir:  # 到达了文件系统的根目录
+            raise FileNotFoundError(f"项目根目录 '{root_dir_name}' 未找到")
+        current_dir = parent_dir
+
+
+current_work_dir = os.getcwd()
+project_root = find_project_root(current_work_dir)
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
 from src.util import *
 from src.Layer_class import *
 from src.Model_class import *
-import tensorflow as tf
-import matplotlib.pyplot as plt
 
 mnist = tf.keras.datasets.mnist
 # Load data
@@ -12,7 +32,6 @@ mnist = tf.keras.datasets.mnist
 x_train, x_test = x_train / 255.0, x_test / 255.0  # 归一化
 
 # Create Model
-
 model = Model(
     [
         FlattenLayer(input_shape=(28, 28), name='Flatten'),
@@ -20,27 +39,25 @@ model = Model(
         Layer(10, activation='softmax', name="L2"),
     ], name="my_model")
 
-print(
-    f"W1 shape = {model.dense_array[0].Weights.shape}, b1 shape = {model.dense_array[0].Biases.shape}")
-print(
-    f"W2 shape = {model.dense_array[1].Weights.shape}, b2 shape = {model.dense_array[1].Biases.shape}")
-print(
-    f"W3 shape = {model.dense_array[2].Weights.shape}, b3 shape = {model.dense_array[2].Biases.shape}")
-
 # Load Weights
 W1 = np.load('../../data/minst_data/SoftMax_weights/layer_1_weights.npy', allow_pickle=True)
 b1 = np.load('../../data/minst_data/SoftMax_weights/layer_1_biases.npy', allow_pickle=True)
 W2 = np.load('../../data/minst_data/SoftMax_weights/layer_2_weights.npy', allow_pickle=True)
 b2 = np.load('../../data/minst_data/SoftMax_weights/layer_2_biases.npy', allow_pickle=True)
 
-
 # Copy weights
-model.dense_array[1].set_weights(W1, b1)
-model.dense_array[2].set_weights(W2, b2)
+# tensorflow输出的weights全是转置过的, 因为转置过的运算效率高
+# 为了统一行和列分别代表 神经元数量和feature数量, 设置的时候需要进行转置
+model.dense_array[1].set_weights(W1.T, b1)
+model.dense_array[2].set_weights(W2.T, b2)
+
+print(model.dense_array[0].get_weights().shape)
+print(model.dense_array[1].get_weights().shape)
+print(model.dense_array[2].get_weights().shape)
 
 # Do prediction
 predictions = model.predict(x_test)
-
+print(predictions)
 # Display Prediction
 # 选择要可视化的样本数量
 num_samples = 30
@@ -49,7 +66,6 @@ fig, axes = plt.subplots(1, num_samples, figsize=(20, 2))
 for i, ax in enumerate(axes):
     # 选择一个随机索引
     idx = np.random.choice(x_test.shape[0])
-
     # 绘制图像
     ax.imshow(x_test[idx], cmap='gray')
     ax.axis('off')
