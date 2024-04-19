87eef548b977fb7339201717c9cf3ea0b5a07011
Johnny <yufengzhang483@gmail.com>
2024-04-19 18:56:39 -0400

    move src codes to other directory

---
+++ src/miniflow/activation.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def linear_function(x_train, w, b):
+    return np.dot(x_train, w.T) + b
+
+
+def sigmoid_function(z):
+    g = 1 / (1 + np.exp(-z))
+    return g
+
+
+def relu_function(z):
+    return np.maximum(0, z)
