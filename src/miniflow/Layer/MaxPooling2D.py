ccc0c68a27bd9e5ee76934050b87ae11b3209b48
Code-MonkeyZhang <yufengzhang483@gmail.com>
2024-07-31 22:40:18 -0400

    early implementation of MaxPooling2D class

---
+++ src/miniflow/Layer/MaxPooling2D.py
@@ -0,0 +1,9 @@
+from .Layer_class import Layer
+from ..util import *
+from ..activation import relu_function
+import numpy as np
+
+class MaxPooling2D(Layer):
+    def __init__(self,pool_size,input_shape, layer_name='layer'):
+        super().__init__(layer_name)
+        self.input_shape = input_shape
--- src/miniflow/Layer/__init__.py
+++ src/miniflow/Layer/__init__.py
@@ -1,2 +1,3 @@
 from .Layer_class import *
 from .ConvLayer import *
+from .MaxPooling2D import *
