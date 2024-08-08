Fix backprop function name

src/miniflow/Layer/Layer_class.py
@@ -7,10 +7,13 @@ class Layer:
     def __init__(self, layer_name='layer'):
         self.layer_name = layer_name
 
+    def get_output_shape(self, *args, **kwargs):
+        pass
+
     def compute_layer(self, *args, **kwargs):
         pass
 
-    def get_output_shape(self, *args, **kwargs):
+    def backward_prop(self, *args, **kwargs):
         pass
 
     def count_params(self):
@@ -60,18 +63,20 @@ class Dense(Layer):
             exp_z = np.exp(z - z_max)  # 减去z中的最大值以避免溢出
             a_out = exp_z / np.sum(exp_z, axis=1, keepdims=True)  # 按行计算softmax
         else:
-            raise ValueError(f"Unsupported activation function: {self.activation}")
+            raise ValueError(
+                f"Unsupported activation function: {self.activation}")
         return a_out
 
-    def forward_prop(self, prev_layer_output, curr_layer_output, label, alpha, b1, b2, epsilon,
-                     backprop_gradient, iter_num) -> np.ndarray:
+    def backward_prop(self, prev_layer_output, curr_layer_output, label, alpha, b1, b2, epsilon,
+                      backprop_gradient, iter_num) -> np.ndarray:
 
         # 对于最后一层 softmax，cost function的求导就是标签相减
         # 这个计算以后要独立出来，目前先放在这里
         cost_func_gradient = np.subtract(curr_layer_output, label)
 
         # obtain gradients of weights and bias for updates
-        dl_dw, dj_db, dl_dz = self.compute_gradient(prev_layer_output, cost_func_gradient, backprop_gradient)
+        dl_dw, dj_db, dl_dz = self.compute_gradient(
+            prev_layer_output, cost_func_gradient, backprop_gradient)
 
         # 根据 chain rule, 传给上一层的gradient应该是:
         # dl/da = dl/ds * ds/da
@@ -86,14 +91,17 @@ class Dense(Layer):
         vdb_corrected = self.Biases_Velocity / (1 - b1 ** iter_num)
 
         # RMS prop
-        self.Squared_Weights = b2 * self.Squared_Weights + (1 - b2) * np.square(vdw_corrected)
-        self.Squared_Biases = b2 * self.Squared_Biases + (1 - b2) * np.square(vdb_corrected)
+        self.Squared_Weights = b2 * self.Squared_Weights + \
+            (1 - b2) * np.square(vdw_corrected)
+        self.Squared_Biases = b2 * self.Squared_Biases + \
+            (1 - b2) * np.square(vdb_corrected)
 
         sdw_corrected = self.Squared_Weights / (1 - b2 ** iter_num)
         sdb_corrected = self.Squared_Biases / (1 - b2 ** iter_num)
 
         # perform gradient descent, update gradient
-        self.Weights -= alpha * vdw_corrected / np.sqrt(sdw_corrected + epsilon)
+        self.Weights -= alpha * vdw_corrected / \
+            np.sqrt(sdw_corrected + epsilon)
         self.Biases -= alpha * vdb_corrected / np.sqrt(sdb_corrected + epsilon)
 
         return backprop_gradient

src/miniflow/Model_class.py
@@ -23,6 +23,7 @@ class Model:
         self.show_summary = None
         self.plot_loss = None
         self.alpha_decay = None
+        self.loss_method = None
 
     # Iterate through each layer, and puts its output to the next layer
     def predict(self, x: np.ndarray) -> np.ndarray:
@@ -33,11 +34,12 @@ class Model:
             prev_layer_output = layer_output
         return prev_layer_output
 
-    def compile(self, optimizer=None, alpha_decay=True, show_summary=False, plot_loss=False):
+    def compile(self, optimizer=None, alpha_decay=True, show_summary=False, plot_loss=False, loss_method=""):
         self.optimizer = optimizer
         self.show_summary = show_summary
         self.plot_loss = plot_loss
         self.alpha_decay = alpha_decay
+        self.loss_method = loss_method
 
     def fit(self, X_train, y_train, learning_rate, epochs, batch_size=32,
             b1=0.2,
@@ -79,8 +81,8 @@ class Model:
 
                 self.layers_output.clear()  # Clear the layers_output before Start
                 prediction = self.predict(train_example)
-                # error = compute_error(prediction=prediction, label=label)
-                error = compute_cross_entropy_loss(prediction, label)
+                error = self.compute_error(
+                    prediction=prediction, label=label, loss_method=self.loss_method)
                 epoch_lost += error
 
                 ############################## START TRAINING ########################################
@@ -95,7 +97,7 @@ class Model:
                     layer_type = type(layer).__name__
                     if layer_type == "FlattenLayer":
                         break  # ignore Flatten layer
-                    backprop_gradient = layer.forward_prop(prev_layer_output,
+                    backprop_gradient = layer.backward_prop(prev_layer_output,
                                                            prediction,
                                                            label,
                                                            learning_rate,
@@ -117,6 +119,10 @@ class Model:
             train_summary(loss=epoch_lost_list, time=epoch_time_list)
         if self.plot_loss:
             plot_loss(epoch_lost_list)
+
+    def compute_error(self, prediction, label, loss_method):
+        if loss_method == "categorical_crossentropy":
+            return compute_cross_entropy_loss(prediction, label)
 
     def set_rand_weight(self, method='He'):
         for layer in self.layers_array: