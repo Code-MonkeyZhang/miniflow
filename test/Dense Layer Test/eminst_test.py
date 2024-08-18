a3f997d5f24d8cbe1f51e250d36cfee97fea584a
Code-MonkeyZhang <yufengzhang483@gmail.com>
2024-08-17 20:37:28 -0400

    correct layer name

--- test/Dense Layer Test/eminst_test.py
+++ test/Dense Layer Test/eminst_test.py
@@ -38,9 +38,10 @@ model = Model(
     [
         FlattenLayer(input_shape=(28, 28, 1), layer_name='Flatten'),
         Dense(256, activation="relu", layer_name="L1", input_shape=784),
-        Dense(128, activation="relu", layer_name="L1", input_shape=256),
-        Dense(64, activation="relu", layer_name="L2", input_shape=128),
-        Dense(47, activation='softmax', layer_name="L3", input_shape=64),  # 注意：这里的输出层单元数应与标签数量一致
+        Dense(128, activation="relu", layer_name="L2", input_shape=256),
+        Dense(64, activation="relu", layer_name="L3", input_shape=128),
+        Dense(47, activation='softmax', layer_name="L4",
+              input_shape=64),  # 注意：这里的输出层单元数应与标签数量一致
     ], name="emnist_model", cost="softmax")
 
 model.set_rand_weight(method='He')
@@ -55,9 +56,9 @@ model.compile(optimizer='adam',
               )
 
 # 训练模型
-model.fit(x_train, y_train, learning_rate=5e-5, epochs=5, batch_size=32, b1=0.9)
+model.fit(x_train, y_train, learning_rate=5e-5,
+          epochs=10, batch_size=32, b1=0.9)
 predictions = model.predict(x_test)
-
 # 计算准确率
 predictions = np.argmax(predictions, axis=1)
 accuracy = np.mean(predictions == y_test)
