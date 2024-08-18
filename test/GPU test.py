1454b35ab006c7f22d7255221429e3e72bf0845f
Code-MonkeyZhang <yufengzhang483@gmail.com>
2024-08-18 11:33:09 -0400

    add run all tests script

--- test/GPU test.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-print("TensorFlow version:", tf.__version__)
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-if tf.config.list_physical_devices('GPU'):
-    print("GPU is available")
-else:
-    print("GPU is NOT available")
